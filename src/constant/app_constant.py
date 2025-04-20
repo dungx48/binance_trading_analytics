26b374be9c558e0b7bb65c5aae7326bc4171a519
dungx48 <dung.mstk56@gmail.com>
2025-04-20 15:40:25 +0700

    [dungpv] edit time run job

--- src/constant/app_constant.py
+++ src/constant/app_constant.py
@@ -3,7 +3,7 @@ from typing import Final
 class AppConst:
     ONE_DAY: Final[str] = '1d'
     USDT: Final[str] = 'USDT'
-    SEVEN_AM: Final[str] = '12:34'
+    SEVEN_AM: Final[str] = '07:00'
     REDIS_KLINES_QUEUE = "klines_queue"
 
 class MODE_RUN_JOB:
