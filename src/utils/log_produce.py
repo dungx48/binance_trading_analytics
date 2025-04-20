56896a7ec37cdc3ba27f6c93efc037a435e3c8f7
dungx48 <dung.mstk56@gmail.com>
2025-04-20 15:37:41 +0700

    [dungpv] cd project

--- src/utils/log_produce.py
+++
@@ -1,6 +0,0 @@
-from utils.logging_config import setup_logging
-import logging
-
-# khởi tạo khi import module
-setup_logging(prefix="produce_klines", level=logging.INFO)
-logger = logging.getLogger(__name__)
